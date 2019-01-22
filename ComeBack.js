43553dfd53645a0e659eb21c090350769905d40a
Clay Hefner <clayhefner@gmail.com>
2019-01-22 18:14:36 -0500

    Add JS for a bad joke

---
+++ ComeBack.js
@@ -0,0 +1,11 @@
+//Shitty js for how many days Clay can survive without Joel//
+
+var response 
+
+function howManyDaysTeamCanLastWithoutJoel(days){
+	if (days <= 1) {response = "We're fine, Clay has this!!!";}
+  else {response = "JOEL HURRY BACK, CLAY IS STARTING TO WRITE BAD JAVASCRIPT.  CHRIS IS VISIBILY CRYING!!!";}
+  return response
+}
+
+console.log(howManyDaysTeamCanLastWithoutJoel())
